14ef08d1b772efceda86ddfec3eaed9339380950
cinemadoc <165213603+cineamdoc@users.noreply.github.com>
2024-06-05 16:07:13 +0900

    Update server.js

--- api/server.js
+++ api/server.js
@@ -8,7 +8,10 @@ import requestsRoute from './routes/requests.js';
 dotenv.config();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true
+}));
 app.use(cookieParser());
 app.use(express.json());
 
